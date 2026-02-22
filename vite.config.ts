7893527781b04884a64b9a3e2ab6d4670f83b198
linkroots-kobe <shotamaehara8190@gmail.com>
2026-02-22 11:33:29 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
-    // GitHub Pages用に相対パスを設定
-    base: './', 
+   base: '/linkrootshomepage/',
     define: {
       // process.env.API_KEY をコード内で使えるように置換設定
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
